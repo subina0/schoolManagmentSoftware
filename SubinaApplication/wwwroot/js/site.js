dde92ff9ae60816332e0977d8a178de4a7247120
subina dhakal <subiakal1212@gmail.com>
2022-09-01 20:48:35 +0545

    initial commit

---
+++ SubinaApplication/wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*var entry = document.getElementsById("entry");
+entry.addEventListener("click", displayDetails);
+var row = 1;
+function displayDetails() {
+    var Name = document.getElementById("name").value;
+    var Age = document.getElementById("age").value;
+    var Fathers_Name = document.getElementById("father").value;
+    var Mothers_Name = document.getElementById("mother").value;
+    var Fathers_occ = document.getElementById("fathocc").value;
+    var Mothers_occ = document.getElementById("mothocc").value;
+    var dob = document.getElementById("dob").value;
+    var Province = document.getElementById("prov").value;
+    var Gender = document.getElementById("gende").value;
+    var Email = document.getElementById("email").value;
+    var Phone_no = document.getElementById("contact").value;
+    var Address = document.getElementById("address").value;
+    var District = document.getElementById("district").value;
+    var Ward = document.getElementById("ward").value;
+    var display = document.getElementById("display");
+    var newrow = display.insertRow(row);
+
+
+    var cell1 = new.insertCell(0);
+    var cell2 = new.insertCell(1);
+    var cell3 = new.insertCell(2);
+    var cell4 = new.insertCell(3);
+
+    cell1.innerHTML = Name;
+    cell2.innerHTML = Age;
+    cell3.innerHTML = Fathers_Name;
+    cell4.innerHTML = Mothers_Name;
+    cell5.innerHTML = Fathers_occ;
+    cell6.innerHTML = Mothers_occ;
+    cell7.innerHTML = dob;
+    cell8.innerHTML = Province;
+    cell9.innerHTML = Gender;
+    cell10.innerHTML = Email;
+    cell11.innerHTML = Phone_no;
+    cell12.innerHTML = Address;
+    cell13.innerHTML = District;
+    cell14.innerHTML = Ward;
+    row++;
+}*/
